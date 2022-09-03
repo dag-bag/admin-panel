98084656188c2fb11a5b195c9c7779f0512edbb5
dag-bag <virenderkumar23435@gmail.com>
2022-09-03 17:14:38 +0530

    hello

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+/** @format */
+
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("postcss-advanced-variables"),
+    require("tailwindcss/nesting"),
+    require("tailwindcss")("./tailwind.config.js"),
+    require("autoprefixer"),
+  ],
+};
